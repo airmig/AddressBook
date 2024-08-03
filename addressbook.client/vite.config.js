beb9b02cb02955a9e829215c8dc6b83989b059a6
Ariel Duran <airmig@gmail.com>
2024-08-03 00:21:55 -0600

    Initial Stub

---
+++ addressbook.client/vite.config.js
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import plugin from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+import child_process from 'child_process';
+import { env } from 'process';
+
+const target = 'http://localhost:5264';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [plugin()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        proxy: {
+            '^/weatherforecast': {
+                target,
+                secure: false
+            }
+        },
+        port: 5173
+    }
+})
